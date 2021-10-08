841b1cb4dd104e1e26fa2d18cbcca399a4efcd40
Mariana Sardi <mariana.sardi@student.ie.edu>
2021-10-08 16:41:11 +0200

    fix conflict

--- Session2.py
+++ Session2.py
@@ -3,11 +3,7 @@ def func(a,b):
     return "hola mundo"
 
 def func2(a,b):
-<<<<<<< HEAD
-    return a/b 
-=======
     return a**b
->>>>>>> 1c8fe4b0663917dc0359f5f904a533f5647f8535
 
 def func3(a):
     return a+3
